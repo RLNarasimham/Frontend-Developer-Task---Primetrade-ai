16255d0596d79b7349d28cd22dc510c2be4999a3
Narasimham <narasimham@mail.com>
2025-10-02 17:38:10 +0530

    Updated userRoutes.js file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,23 @@
+// const express = require('express');
+// const router = express.Router();
+// const {
+//     registerUser,
+//     loginUser,
+//     getUserProfile,
+//     updateUserProfile,
+//     forgotPassword,
+// } = require('../controllers/userController');
+// const { protect } = require('../middleware/authMiddleware');
+
+// router.post('/register', registerUser);
+// router.post('/login', loginUser);
+// router.post('/forgot-password', forgotPassword);
+
+
+// router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const {
@@ -9,11 +29,20 @@ const {
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/forgot-password', forgotPassword);
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+});
 
-
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/register').post(registerUser).options((req, res) => res.sendStatus(204));
+router.route('/login').post(loginUser).options((req, res) => res.sendStatus(204));
+router.route('/forgot-password').post(forgotPassword).options((req, res) => res.sendStatus(204));
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile).options((req, res) => res.sendStatus(204));
 
 module.exports = router;
