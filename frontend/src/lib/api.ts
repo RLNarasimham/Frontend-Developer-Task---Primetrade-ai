c459e38167658b3036783ddfc0c9eb1a87d77898
Narasimham <narasimham@mail.com>
2025-10-02 16:32:16 +0530

    Updated a file

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,11 +1,29 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// });
+
+// api.interceptors.request.use((config) => {
+//   const user = JSON.parse(localStorage.getItem('user') || '{}');
+//   if (user && user.token) {
+//     config.headers.Authorization = `Bearer ${user.token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
   if (user && user.token) {
     config.headers.Authorization = `Bearer ${user.token}`;
   }
