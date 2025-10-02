d746d6ceccf90b7a2861117539e5c4d236e6d1e1
Narasimham <narasimham@mail.com>
2025-10-02 17:37:01 +0530

    Updated taskRoutes.js file

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,37 @@
+// const express = require('express');
+// const router = express.Router();
+// const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+// const { protect } = require('../middleware/authMiddleware');
+
+// router.route('/').get(protect, getTasks).post(protect, createTask);
+// router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 
+router.use(express.json());
+router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+});
+
+const validateId = (req, res, next) => {
+    const id = req.params.id;
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'Invalid id' });
+    next();
+};
+
 router.route('/').get(protect, getTasks).post(protect, createTask);
-router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
+router.put('/:id', protect, validateId, updateTask);
+router.delete('/:id', protect, validateId, deleteTask);
 
 module.exports = router;
